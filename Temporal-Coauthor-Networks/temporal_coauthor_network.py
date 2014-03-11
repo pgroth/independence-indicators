6c5592f6544b2f7ec4c1855de961d97ec935c8d6
Luka Stout <lukastout@gmail.com>
2014-03-11 09:04:47 +0100

    Added vincent support

--- Temporal-Coauthor-Networks/temporal_coauthor_network.py
+++ Temporal-Coauthor-Networks/temporal_coauthor_network.py
@@ -1,44 +1,55 @@
 import networkx as nx
 from temporal_edge import TemporalEdge
 class TemporalCoAuthorNetwork(object):
-    
-    def __init__(self):
-        """ Initialize the TemporalCoAuthorNetwork """
-        self.edges = []
-            
-    def __repr__(self):
-        return str(self.edges)
-    
-    def addEdge(self,node1,node2,w,t):
-        """
-            Add an edge between node1 and node2 with weight w at time t
-            params:
-                node1 : string
-                            Name of the node
-                node2 : string
-                            Name of the node
-                w : int
-                            The weight
-                t : int
-                            The timepoint
-        """
-        node1, node2 = min(node1,node2),max(node1,node2)
-        if (node1,node2) not in self.edges:
-            edge = TemporalEdge(node1,node2)
-            self.edges.append(edge)
-        i = self.edges.index((node1,node2))
-        self.edges[i].add(w,t)
-    
-    def networkAtTime(self,t):
-        """
-            Get a networkx Graph-object representation of the network at time 't'
-            params:
-                t : int
-                        The time at which to show the network
-        """
-        N = nx.Graph()
-        for edge in self.edges:
-            w = edge.weight(t)
-            if w:
-                N.add_edge(*edge.nodes,weight = w)
-        return N
+	
+	def __init__(self):
+		""" Initialize the TemporalCoAuthorNetwork """
+		self.edges = []
+			
+	def __repr__(self):
+		return str(self.edges)
+	
+	def addEdge(self,node1,node2,w,t):
+		"""
+			Add an edge between node1 and node2 with weight w at time t
+			params:
+				node1 : string
+							Name of the node
+				node2 : string
+							Name of the node
+				w : int
+							The weight
+				t : int
+							The timepoint
+		"""
+		node1, node2 = min(node1,node2),max(node1,node2)
+		if (node1,node2) not in self.edges:
+			edge = TemporalEdge(node1,node2)
+			self.edges.append(edge)
+		i = self.edges.index((node1,node2))
+		self.edges[i].add(w,t)
+	
+	def networkAtTime(self,t):
+		"""
+			Get a networkx Graph-object representation of the network at time 't'
+			params:
+				t : int
+						The time at which to show the network
+		"""
+		N = nx.Graph()
+		for edge in self.edges:
+			w = edge.weight(t)
+			if w:
+				N.add_edge(*edge.nodes,weight = w)
+		return N
+		
+	def network(self):
+		"""
+			Get a networkx Graph-object representation of the network at the end of time
+		"""
+		N = nx.Graph()
+		for edge in self.edges:
+			w = edge.end_weight()
+			if w:
+				N.add_edge(*edge.nodes,weight = w)
+		return N
--- Temporal-Coauthor-Networks/temporal_edge.py
+++ Temporal-Coauthor-Networks/temporal_edge.py
@@ -1,58 +1,74 @@
 class TemporalEdge(object):
-    """ The edges used in the Temporal Network
-        Every edge is unique by it's nodes.
-    """
-    
-    def __init__(self,node1,node2):
-        """ Initialize the edge without weights
-            params:
-                node1 : string
-                            Name of the node
-                node2 : string
-                            Name of the node
-        """
-        self.node1 = node1
-        self.node2 = node2
-        self.occurences = dict()
-    
-    def add(self, weight, time):
-        """ Adds the weight to the edge at timepoit 'time'
-            params:
-                weight : int
-                            The added weight
-                time : int
-                            The timepoint
-        """
-        if time not in self.occurences:
-            self.occurences[time] = weight
-        else:
-             self.occurences[time] += weight
-    
-    @property
-    def nodes(self):
-        """ Return a tuple of involved nodes of the edge"""
-        return (self.node1,self.node2)
-    
-    def weight(self,t):
-        """ Get the weight of the edge at timePoint 't'
-            params: 
-                t : int
-                        The timepoint
-        
-        """
-        curDict = {time : weight for time,weight in self.occurences.items() if time <= t}
-        w = 0
-        for (time,weight) in curDict.items():
-            w += weight * 0.9**(t - time)
-        return w
-        
-    def __eq__(self,other):
-        if isinstance(other,type(self)):
-            return other.node1 == self.node1 and other.node2 == self.node2 and other.occurences == self.occurences
-        elif isinstance(other,tuple):
-            if len(other) == 2:
-                return self.node1 == other[0] and self.node2 == other[1]
-        return False
-    
-    def __repr__(self):
-        return "%s -> %s %s" % (self.node1,self.node2,self.occurences)
+	""" The edges used in the Temporal Network
+		Every edge is unique by it's nodes.
+	"""
+	
+	def __init__(self,node1,node2):
+		""" Initialize the edge without weights
+			params:
+				node1 : string
+							Name of the node
+				node2 : string
+							Name of the node
+		"""
+		self.node1 = node1
+		self.node2 = node2
+		self.occurences = dict()
+	
+	def add(self, weight, time):
+		""" Adds the weight to the edge at timepoit 'time'
+			params:
+				weight : int
+							The added weight
+				time : int
+							The timepoint
+		"""
+		if time not in self.occurences:
+			self.occurences[time] = weight
+		else:
+			 self.occurences[time] += weight
+	
+	@property
+	def nodes(self):
+		""" Return a tuple of involved nodes of the edge"""
+		return (self.node1,self.node2)
+	
+	def weight(self,t):
+		""" Get the weight of the edge at timePoint 't'
+			params: 
+				t : int
+						The timepoint
+		
+		"""
+		cur_dict = {time : weight for time,weight in self.occurences.items() if time <= t}
+		w = 0
+		for (time,weight) in cur_dict.items():
+			w += weight * 0.9**(t - time)
+		if w < 1:
+					w = 0
+		return w
+		
+	def end_weight(self):
+		""" Get the weight of the edge after all links have been added
+			params: 
+				t : int
+						The timepoint
+		
+		"""
+		cur_dict = {time : weight for time,weight in self.occurences.items()}
+		max_time = max([time for time,weight in self.occurences.items()])
+		w = 0
+		for (time,weight) in cur_dict.items():
+			w += weight * 0.9**(max_time - time)
+		return w
+		
+	def __eq__(self,other):
+		if isinstance(other,type(self)):
+			return other.node1 == self.node1 and other.node2 == self.node2 and other.occurences == self.occurences
+		elif isinstance(other,tuple):
+			if len(other) == 2:
+				return self.node1 == other[0] and self.node2 == other[1]
+		return False
+	
+	def __repr__(self):
+		return "%s -> %s %s" % (self.node1,self.node2,self.occurences)
